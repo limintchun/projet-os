af94dc5bf575035a4db5647cb43933a03df596e4
Soulsliked01 <145702047+Soulsliked01@users.noreply.github.com>
2024-11-04 17:16:47 +0100

    Update main.c
    Voir si la structure peut être correcte comparée à la vôtre

--- src/main.c
+++ src/main.c
@@ -1,5 +1,33 @@
+#include <stdio.h> //biblio standart
+//processus
+#include <sys/types.h> //PID
+#include <unistd.h> //Fork(), exec(), pipes
+#include <sys/wait.h> //Eventuel pour mettre en pause le processus
+//Threads
+#include <pthread.h>
+//Strings ?
+#include <string.h>
+
+int chat(char pseudo_utilisateur[30],char pseudo_destinataire[30], param1, param2 ){
+  if(param1 != NULL){
+      TODO
+      //cas --bot
+
+  }else if(param2 != NULL){
+      TODO
+      //cas manuel
+  }
 
 
+}
 int main(int argc, char* argv[]) {
+  /* Tous les appels systèmes devront être placé dans main()
+  Tandis que "chat" sera considéré comme processus et ne gérera que
+  le comportement.
+
+  */
+
    return 0;
 }
+
+
