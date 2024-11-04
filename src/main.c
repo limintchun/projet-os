c7faace21d1d6e64b68430ca4fc497a3a5ac77a1
Ayman-kahch <120568324+Ayman-kahch@users.noreply.github.com>
2024-11-04 15:59:13 +0100

    Update main.c
    Modification des tests pour argv

--- src/main.c
+++ src/main.c
@@ -1,5 +1,25 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctype.h>
+
 
 
 int main(int argc, char* argv[]) {
+   if (1 < argc) {
+      for (int i = 1; i < 3; i++) {
+         // On ne regarde que les deux premiers params. argv[1] et argv[2] correspondant aux pseudonymes
+         for (size_t j = 0; j < strlen(argv[i]); j++) {
+            // On parcourt chaque caractere j de argv[i] avec argv[i][j]
+            // On appelle ispunct() pour verifier s'il y a un caractere de ponctuation dans chaque param.
+            // size_t pour la variable j == type de retour de strlen() (long unsigned int)
+            if (ispunct((unsigned char)argv[i][j])) {
+               fprintf(stderr, "Error : punctuation characters are forbidden.");
+               return 3;
+            }
+         }
+      }
+   }
    return 0;
 }
