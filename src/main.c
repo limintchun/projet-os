5c8fdac7ea8db8786389f1b169d89f0796dbc743
Ayman-kahch <120568324+Ayman-kahch@users.noreply.github.com>
2024-11-05 17:41:01 +0100

    Update main.c
    J'ai add les conventions pour ecrire ;
    CamelCase pour les structures
    camelCase pour les fonctions
    under_score pour les variables
    
    J'ai ajoute une fonction qui s'occuper de faire les check pour les argv afin de rendre le main plus clean
    
    Je permets egalement de mettre comme username UNIQUEMENT en argv[1] et argv[2] --bot ou --manuel , pour l'instant il y a continue mais on peut faire ce qu'on veut
    
    Vous remarquerez aussi que ma fonction modifie les varibles du main car je les donne par pointeurs (* et &)

--- src/main.c
+++ src/main.c
@@ -9,7 +9,13 @@
 #include <stdlib.h>
 
 
+// CamelCase for struct
+// camelCase for function
+// under_score for variable
+
+
 #define MAX_LENGHT_USERNAME 30
+#define DICTIONARY_SIZE 6
 #define BOT_MODE "--bot"
 #define MANUAL_MODE "--manuel"
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -21,14 +27,63 @@
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
 
-void my_handler(int received_signal);
+int checkVadlidArgv(int argc, char* argv[], bool* bot_mode_activated, bool* manual_mode_activated);
+void myHandler(int received_signal);
 
 
-// returnValueType parseCheckValidParam(int argc, char* argv[]) {
-// }
+int checkVadlidArgv(int argc, char* argv[], bool* bot_mode_activated, bool* manual_mode_activated) {
+   if (1 == argc)  {
+      fprintf(stderr, "chat pseudo_utilisateur pseudo_destinataire [--bot] [--manuel]\n");
+      return 1;
+   }
+   else {
+      for (int i = 1; i < 3; i++) {
+         // On ne regarde que les deux premiers params. argv[1] et argv[2] correspondant aux pseudonymes
+         if (strlen(argv[i]) > MAX_LENGHT_USERNAME) {
+            fprintf(stderr, "Error : the maximum length of usernames is 30.\n");
+            return 2;
+         }
+         // CHECK IF --PARAM IN ARGV[1] OR ARGV[2] SHOULD RISE AN ERROR OR BE CONSIDERED AS AN USERNAME
+         else if (strcmp(argv[i], "--bot") == 0) {
+            continue;
+         } 
+         else if (strcmp(argv[i], "--manuel") == 0) {
+            continue;
+         }
+         else {
+            for (size_t j = 0; j < strlen(argv[i]); j++) {
+            // On parcourt chaque caractere j de argv[i] avec argv[i][j]
+            // On appelle ispunct() pour verifier s'il y a un caractere de ponctuation dans chaque param.
+            // size_t pour la variable j == type de retour de strlen() (long unsigned int)
+               if (ispunct((unsigned char)argv[i][j])) {
+                  fprintf(stderr, "Error : punctuation characters are forbidden for usernames.\n");
+                  return 3;
+               }
+            }
+         }  
+      }
+      if (argc == 4) {
+         if (strcmp(argv[3], BOT_MODE) == 0) {
+            *bot_mode_activated = true;
+         }
+         else if (strcmp(argv[3], MANUAL_MODE) == 0) {
+            *manual_mode_activated = true;
+         }
+      }
+      else if (argc == 5) {
+         if (strcmp(argv[3], BOT_MODE) == 0 || strcmp(argv[4], BOT_MODE) == 0) {
+            *bot_mode_activated = true;
+         }
+         if (strcmp(argv[3], MANUAL_MODE) == 0 || strcmp(argv[4], MANUAL_MODE) == 0) {
+            *manual_mode_activated = true;
+         }
+      }
+   }
+   return 0;
+}
 
 
-void my_handler(int received_signal) {
+void myHandler(int received_signal) {
    if (received_signal == SIGINT) {
       printf("The user pressed CTRL+C.\n");
       exit(0);
@@ -39,47 +94,61 @@ void my_handler(int received_signal) {
 int main(int argc, char* argv[]) {
    bool bot_mode_activated = false;
    bool manual_mode_activated = false;
-// CHECK IF --PARAM IN ARGV[1] OR ARGV[2] SHOULD RISE AN ERROR OR BE CONSIDERED AS AN USERNAME
-   if (1 < argc) {
-      for (int i = 1; i < 3; i++) {
-         // On ne regarde que les deux premiers params. argv[1] et argv[2] correspondant aux pseudonymes
-         if (strlen(argv[i]) > MAX_LENGHT_USERNAME) {
-            fprintf(stderr, "Error : the maximum length of usernames is 30.\n");
-            return 2;
-         }
-         for (size_t j = 0; j < strlen(argv[i]); j++) {
-            // On parcourt chaque caractere j de argv[i] avec argv[i][j]
-            // On appelle ispunct() pour verifier s'il y a un caractere de ponctuation dans chaque param.
-            // size_t pour la variable j == type de retour de strlen() (long unsigned int)
-            if (ispunct((unsigned char)argv[i][j])) {
-               fprintf(stderr, "Error : punctuation characters are forbidden for usernames.\n");
-               return 3;
-            }
-         }
-      }
-      if (argc == 4) {
-         if (!strcmp(argv[3], BOT_MODE)) {
-            bot_mode_activated = true;
-         }
-         else if (!strcmp(argv[3], MANUAL_MODE)) {
-            manual_mode_activated = true;
-         }
-      }
-      else if (argc == 5) {
-         if (!strcmp(argv[3], BOT_MODE) || !strcmp(argv[4], BOT_MODE)) {
-            bot_mode_activated = true;
-         }
-         if (!strcmp(argv[3], MANUAL_MODE) || !strcmp(argv[4], MANUAL_MODE)) {
-            manual_mode_activated = true;
-         }
-      }
-   }
-   else {
-      fprintf(stderr, "chat pseudo_utilisateur pseudo_destinataire [--bot] [--manuel]\n");
-      return 1;
+   int result_checking = checkVadlidArgv(argc, argv, &bot_mode_activated, &manual_mode_activated);
+   if (result_checking != 0) {
+      return result_checking;
    }
    if (manual_mode_activated) {
-      signal(SIGINT, my_handler);
+      signal(SIGINT, myHandler);
    }
    return 0;
 }
+
+
+// int main(int argc, char* argv[]) {
+//    bool bot_mode_activated = false;
+//    bool manual_mode_activated = false;
+//    if (1 == argc)  {
+//       fprintf(stderr, "chat pseudo_utilisateur pseudo_destinataire [--bot] [--manuel]\n");
+//       return 1;
+//    }
+//    else {
+//    // CHECK IF --PARAM IN ARGV[1] OR ARGV[2] SHOULD RISE AN ERROR OR BE CONSIDERED AS AN USERNAME
+//       for (int i = 1; i < 3; i++) {
+//          // On ne regarde que les deux premiers params. argv[1] et argv[2] correspondant aux pseudonymes
+//          if (strlen(argv[i]) > MAX_LENGHT_USERNAME) {
+//             fprintf(stderr, "Error : the maximum length of usernames is 30.\n");
+//             return 2;
+//          }
+//          for (size_t j = 0; j < strlen(argv[i]); j++) {
+//             // On parcourt chaque caractere j de argv[i] avec argv[i][j]
+//             // On appelle ispunct() pour verifier s'il y a un caractere de ponctuation dans chaque param.
+//             // size_t pour la variable j == type de retour de strlen() (long unsigned int)
+//             if (ispunct((unsigned char)argv[i][j])) {
+//                fprintf(stderr, "Error : punctuation characters are forbidden for usernames.\n");
+//                return 3;
+//             }
+//          }
+//       }
+//       if (argc == 4) {
+//          if (!strcmp(argv[3], BOT_MODE)) {
+//             bot_mode_activated = true;
+//          }
+//          else if (!strcmp(argv[3], MANUAL_MODE)) {
+//             manual_mode_activated = true;
+//          }
+//       }
+//       else if (argc == 5) {
+//          if (!strcmp(argv[3], BOT_MODE) || !strcmp(argv[4], BOT_MODE)) {
+//             bot_mode_activated = true;
+//          }
+//          if (!strcmp(argv[3], MANUAL_MODE) || !strcmp(argv[4], MANUAL_MODE)) {
+//             manual_mode_activated = true;
+//          }
+//       }
+//    }
+//    if (manual_mode_activated) {
+//       signal(SIGINT, my_handler);
+//    }
+//    return 0;
+// }
