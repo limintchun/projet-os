4b01e29c6d0021cf4a52dc5390620a157b73e2c9
Ayman-kahch <120568324+Ayman-kahch@users.noreply.github.com>
2024-11-05 19:39:30 +0100

    Update main.c

--- src/main.c
+++ src/main.c
@@ -15,7 +15,6 @@
 
 
 #define MAX_LENGHT_USERNAME 30
-#define DICTIONARY_SIZE 6
 #define BOT_MODE "--bot"
 #define MANUAL_MODE "--manuel"
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -103,52 +102,3 @@ int main(int argc, char* argv[]) {
    }
    return 0;
 }
-
-
-// int main(int argc, char* argv[]) {
-//    bool bot_mode_activated = false;
-//    bool manual_mode_activated = false;
-//    if (1 == argc)  {
-//       fprintf(stderr, "chat pseudo_utilisateur pseudo_destinataire [--bot] [--manuel]\n");
-//       return 1;
-//    }
-//    else {
-//    // CHECK IF --PARAM IN ARGV[1] OR ARGV[2] SHOULD RISE AN ERROR OR BE CONSIDERED AS AN USERNAME
-//       for (int i = 1; i < 3; i++) {
-//          // On ne regarde que les deux premiers params. argv[1] et argv[2] correspondant aux pseudonymes
-//          if (strlen(argv[i]) > MAX_LENGHT_USERNAME) {
-//             fprintf(stderr, "Error : the maximum length of usernames is 30.\n");
-//             return 2;
-//          }
-//          for (size_t j = 0; j < strlen(argv[i]); j++) {
-//             // On parcourt chaque caractere j de argv[i] avec argv[i][j]
-//             // On appelle ispunct() pour verifier s'il y a un caractere de ponctuation dans chaque param.
-//             // size_t pour la variable j == type de retour de strlen() (long unsigned int)
-//             if (ispunct((unsigned char)argv[i][j])) {
-//                fprintf(stderr, "Error : punctuation characters are forbidden for usernames.\n");
-//                return 3;
-//             }
-//          }
-//       }
-//       if (argc == 4) {
-//          if (!strcmp(argv[3], BOT_MODE)) {
-//             bot_mode_activated = true;
-//          }
-//          else if (!strcmp(argv[3], MANUAL_MODE)) {
-//             manual_mode_activated = true;
-//          }
-//       }
-//       else if (argc == 5) {
-//          if (!strcmp(argv[3], BOT_MODE) || !strcmp(argv[4], BOT_MODE)) {
-//             bot_mode_activated = true;
-//          }
-//          if (!strcmp(argv[3], MANUAL_MODE) || !strcmp(argv[4], MANUAL_MODE)) {
-//             manual_mode_activated = true;
-//          }
-//       }
-//    }
-//    if (manual_mode_activated) {
-//       signal(SIGINT, my_handler);
-//    }
-//    return 0;
-// }
