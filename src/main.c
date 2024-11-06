a39e9c67b667ffb57fdcc19668a694a86c425c05
Soulsliked01 <145702047+Soulsliked01@users.noreply.github.com>
2024-11-06 16:06:36 +0100

    Update gestion des pseudos

--- src/main.c
+++ src/main.c
@@ -1,33 +1,69 @@
-#include <stdio.h> //biblio standart
-//processus
-#include <sys/types.h> //PID
-#include <unistd.h> //Fork(), exec(), pipes
-#include <sys/wait.h> //Eventuel pour mettre en pause le processus
-//Threads
-#include <pthread.h>
-//Strings ?
+#include <stdio.h>
 #include <string.h>
-
-int chat(char pseudo_utilisateur[30],char pseudo_destinataire[30], param1, param2 ){
-  if(param1 != NULL){
-      TODO
-      //cas --bot
-
-  }else if(param2 != NULL){
-      TODO
-      //cas manuel
-  }
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <ctype.h>
+#include <stdbool.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
 
 
+#define MAX_LENGHT_USERNAME 30
+
+
+#define ANSI_COLOR_RED     "\x1b[31m"
+#define ANSI_COLOR_GREEN   "\x1b[32m"
+#define ANSI_COLOR_YELLOW  "\x1b[33m"
+#define ANSI_COLOR_BLUE    "\x1b[34m"
+#define ANSI_COLOR_MAGENTA "\x1b[35m"
+#define ANSI_COLOR_CYAN    "\x1b[36m"
+#define ANSI_COLOR_RESET   "\x1b[0m"
+
+
+void handler(int received_signal) {
+    if (received_signal == SIGINT) {
+      printf("  \n"); //evite que le message d'erreur soit collé à l'éventuel message écrit
+      printf("The user pressed CTRL+C.\n");
+      exit(0);
+    }
 }
+
 int main(int argc, char* argv[]) {
-  /* Tous les appels systèmes devront être placé dans main()
-  Tandis que "chat" sera considéré comme processus et ne gérera que
-  le comportement.
+  if (argc > 1){
 
-  */
+    //check longueur pseudos
+    if (strlen(argv[0]) > TAILLE_MAX || strlen(argv[1]) > TAILLE_MAX) {
+      fprintf(stderr, "un pseudonyme doit contenir maximum 30 caractères\n");
+      return 2;
 
-   return 0;
+    // lien avec code Ayaman checke si paramètres doivent être considéres en pseudos
+    }else if (strcmp(argv[i], "--bot") == 0) {
+       continue;
+
+    }else if (strcmp(argv[i], "--manuel") == 0) {
+       continue;
+
+    }else:
+      for (int num_pseudo = 1; num_pseudo < 3; num_pseudo++) {
+        // Vérification parmi les deux pseudos si des caractères spéciaux sont présents
+        for (int charactere = 0; charactere < strlen(argv[num_pseudo]); charactere++) {
+          char c = argv[num_pseudo][charactere];  // Récupère le caractère actuel
+
+          // Vérification si le caractère est un des symboles recherchés
+          if (c == '/' || c == '-' || c == '[' || c == ']' || c == '.') {
+            fprintf(stderr, "Failure to create username, the pseudo %d  cannot contain the character %c\n",num_pseudo, c);
+            return 3;
+          }
+        }
+      }
+  }
+  if(argc == 4){
+    if(argv[3] == "--bot"){
+
+
+    }
+
+  }
+  return 0;
 }
-
-
