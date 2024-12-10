57972d66bbe66ff437b22bf50ab21b34b15f2c8b
Soulsliked01 <145702047+Soulsliked01@users.noreply.github.com>
2024-12-10 17:13:53 +0100

    Update client.c

--- src/chat/client.c
+++ src/chat/client.c
@@ -1,77 +1,81 @@
 #include <sys/socket.h> // socket(), setsockopt()
-#include <netinet/in.h> // struct sockaddr_in, inaddr_any
-#include <string.h> // strlen()
-#include <unistd.h> // read(), write(), close()
-#include <arpa/inet.h> // inet_pton()
-#include <stdio.h> // printf()
-#include <stdlib.h> // getenv()
-#include <sys/types.h>
+#include <netinet/in.h> // struct sockaddr_in, INADDR_ANY
+#include <string.h>     // strlen()
+#include <unistd.h>     // read(), write(), close()
+#include <arpa/inet.h>  // inet_pton()
+#include <stdio.h>      // printf(), perror()
+#include <stdlib.h>     // getenv(), atoi()
 
 int main() {
-
-    // initialisation du socket
+   
+    // Initialisation du socket
     int sock = socket(AF_INET, SOCK_STREAM, 0);
     if (sock < 0) {
         perror("socket()");
         return 1;
     }
-
-    // initialisation des paramètres de l'adresse
+   
+    // Initialisation des paramètres de l'adresse
     struct sockaddr_in serv_addr;
+    memset(&serv_addr, 0, sizeof(serv_addr)); // Assurez-vous que tous les champs sont bien initialisés
     serv_addr.sin_family = AF_INET;
 
-    // récupération des variables locales
+    // Récupération des variables d'environnement
     char *local_ip = getenv("IP_SERVEUR");
     char *local_port = getenv("PORT_SERVEUR");
 
-    // initialisation de l'adresse ip en fonction de la variable locale
-    // if (local_ip == NULL) {
-    //     printf("IP_SERVEUR non initialisée"); // pour que ça fonctionne, utiliser export VAR=VAL
-    //     return 1;
-    // }
-    // else {
-    //     if (inet_pton(AF_INET, local_ip, &serv_addr.sin_addr) < 0) {
-    //         perror("inet_pton()");
-    //         return 1;
-    //     }
-    //     else if (inet_pton(AF_INET, local_ip, &(serv_addr.sin_addr)) == 1) { // adresse ip valide
-    //         serv_addr.sin_addr.s_addr = inet_pton(AF_INET, local_ip, &serv_addr.sin_addr);
-    //     }
-    //     else { // adresse ip non valide
-    //         serv_addr.sin_addr.s_addr = inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr); 
-    //     }
-    // }
+    // Configuration de l'adresse IP
+    if (local_ip == NULL) {
+        fprintf(stderr, "IP_SERVEUR non initialisée. Utilisation de l'IP par défaut : 127.0.0.1\n");
+        if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+            perror("inet_pton() pour l'adresse par défaut");
+            close(sock);
+            return 1;
+        }
+    } else {
+        if (inet_pton(AF_INET, local_ip, &serv_addr.sin_addr) <= 0) {
+            fprintf(stderr, "Adresse IP non valide. Utilisation de l'IP par défaut : 127.0.0.1\n");
+            if (inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr) <= 0) {
+                perror("inet_pton() pour l'adresse par défaut");
+                close(sock);
+                return 1;
+            }
+        }
+    }
+   
+    // Configuration du port
+    if (local_port == NULL) {
+        fprintf(stderr, "PORT_SERVEUR non initialisé. Utilisation du port par défaut : 1234\n");
+        serv_addr.sin_port = htons(1234);
+    } else {
+        int port = atoi(local_port);
+        if (port >= 1 && port <= 65535) {
+            serv_addr.sin_port = htons(port); // Conversion en format réseau
+        } else {
+            fprintf(stderr, "Port non valide. Utilisation du port par défaut : 1234\n");
+            serv_addr.sin_port = htons(1234);
+        }
+    }
 
-    // initialisation du port en fonction de la variable locale
-     if (local_port == NULL) {
-         printf("PORT_SERVEUR non initialisée");
-         return 1;
-     }
-     else {
-         int port = atoi(local_port); // permet de convertir char vers int
-         if (port >= 1 && port <= 65535) {
-             serv_addr.sin_port = htons(port); // convertit en format réseau
-         }
-         else {
-             serv_addr.sin_port = htons(1234);
-         }
-
-     }
-
-    // connexion au socket
+    // Connexion au serveur
     if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
         perror("connect()");
+        close(sock);
         return 1;
     }
+    printf("Connexion réussie au serveur %s:%d\n",
+           local_ip ? local_ip : "127.0.0.1", ntohs(serv_addr.sin_port));
 
-    // écriture des données
-    char message[] = "SALUT A TOUS";
+    // Envoi des données
+    char message[1024];
+    scanf("%s", message);
     if (write(sock, message, strlen(message)) < 0) {
         perror("write()");
+        close(sock);
         return 1;
     }
-
-    // fermeture du socket
+  // Fermeture du socket
     close(sock);
     return 0;
 }
+
